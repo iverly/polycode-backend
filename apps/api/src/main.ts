9508a57120766ed1cfc33d8f52a90988d9f02b8f
iverly <github@iverly.net>
2022-04-28 19:38:27 +0200

    feat: add helmet to api application
    Signed-off-by: iverly <github@iverly.net>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
 }
