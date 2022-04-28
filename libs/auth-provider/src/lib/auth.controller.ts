36b2e5b588b6a5ccea0aa158903ee060bb024b06
iverly <github@iverly.net>
2022-04-28 19:07:36 +0200

    feat: add auth-provider controller
    Signed-off-by: iverly <github@iverly.net>

---
+++ libs/auth-provider/src/lib/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
+import { OAuth2Service } from './services/oauth2.service';
+import { OAuth2AuthenticateDto } from './dtos/oauth2/authenticate.dto';
+import { AuthService } from './services/auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly oAuth2Service: OAuth2Service,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Post('/token')
+  async token(@Body() oAuth2AuthenticateDto: OAuth2AuthenticateDto) {
+    return this.oAuth2Service.authenticate(oAuth2AuthenticateDto);
+  }
+
+  @Get('/authorize')
+  authorize(@Headers('authorization') authorizationHeader: string) {
+    return this.authService.authorize(authorizationHeader);
+  }
+}
--- libs/auth-provider/src/lib/auth.module.ts
+++ libs/auth-provider/src/lib/auth.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { validate } from './env.validation';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { sequelizeModels } from './models';
+import { AuthService } from './services/auth.service';
+import { OAuth2Service } from './services/oauth2.service';
+import { TokenService } from './services/token.service';
+import { ClientService } from './services/client.service';
+import { SubjectService } from './services/subject.service';
+import { CryptoService } from './services/crypto.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ validate }),
+    JwtModule.register({ secret: process.env.AUTH_JWT_SECRET }),
+    SequelizeModule.forFeature(sequelizeModels),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    OAuth2Service,
+    TokenService,
+    ClientService,
+    SubjectService,
+    CryptoService,
+  ],
   exports: [],
 })
 export class AuthProviderModule {}
