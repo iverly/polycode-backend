4a090ca5d2b5bfd2291f4b7b26280351a621ddae
iverly <github@iverly.net>
2022-04-28 19:16:21 +0200

    feat: add app env validation & sequelize configuration
    Signed-off-by: iverly <github@iverly.net>

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Environment } from '@polycode/env';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate } from './env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ validate }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      logging:
+        process.env.NODE_ENV === Environment.PRODUCTION ? false : console.log,
+      host: process.env.AUTH_DATABASE_HOST,
+      port: Number(process.env.AUTH_DATABASE_PORT),
+      username: process.env.AUTH_DATABASE_USER,
+      password: process.env.AUTH_DATABASE_PASSWORD,
+      database: process.env.AUTH_DATABASE_NAME,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      pool: {
+        max: process.env.NODE_ENV === Environment.PRODUCTION ? 50 : 2,
+        min: process.env.NODE_ENV === Environment.PRODUCTION ? 20 : 2,
+        idle: 5000,
+      },
+      models: [],
+      define: {
+        underscored: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/app/env.validation.ts
@@ -0,0 +1,60 @@
+import { Environment } from '@polycode/env';
+import { plainToClass } from 'class-transformer';
+import { IsEnum, IsNumber, IsString, validateSync } from 'class-validator';
+
+/* It's a class that contains all the environment variables that the application needs to run */
+class EnvironmentVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsString({
+    message: 'The environment variable "AUTH_DATABASE_NAME" must be a string',
+  })
+  AUTH_DATABASE_NAME: string;
+
+  @IsString({
+    message: 'The environment variable "AUTH_DATABASE_USER" must be a string',
+  })
+  AUTH_DATABASE_USER: string;
+
+  @IsString({
+    message:
+      'The environment variable "AUTH_DATABASE_PASSWORD" must be a string',
+  })
+  AUTH_DATABASE_PASSWORD: string;
+
+  @IsString({
+    message: 'The environment variable "AUTH_DATABASE_HOST" must be a string',
+  })
+  AUTH_DATABASE_HOST: string;
+
+  @IsNumber(
+    {},
+    {
+      message: 'The environment variable "AUTH_DATABASE_PORT" must be a number',
+    },
+  )
+  AUTH_DATABASE_PORT: number;
+}
+
+/**
+ * It takes a config object, validates it, and returns the validated object
+ * @param config - The configuration object to validate.
+ * @returns The validated config object.
+ */
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToClass(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  /* This is validating the config object. */
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
