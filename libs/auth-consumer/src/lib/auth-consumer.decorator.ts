711660b6675d092f394baa699438e6f719ffcd65
iverly <github@iverly.net>
2022-04-29 15:05:47 +0200

    feat: fixn metadata mutation for auth-consumer
    Signed-off-by: iverly <github@iverly.net>

--- libs/auth-consumer/src/lib/auth-consumer.decorator.ts
+++ libs/auth-consumer/src/lib/auth-consumer.decorator.ts
@@ -3,9 +3,7 @@ import { SubjectType } from './types/subject.type';
 import { AuthorizationGuard, SubjectTypeGuard } from './auth-consumer.guard';
 import { AUTH_CONSUMER_DECORATOR_SUBJECT_TYPES } from './auth-consumer.constants';
 import { IRolePolicies } from './interfaces';
-import { SetPoliciesHandler, SubjectAbility } from '@polycode/casl';
-import { subject } from '@casl/ability';
-import { Request } from 'express';
+import { SetPolicies } from '@polycode/casl';
 import { PolicyGuard } from '@polycode/casl';
 
 export interface AuthorizeDecoratorOptions {
@@ -21,45 +19,7 @@ export function Authorize(options: AuthorizeDecoratorOptions = {}) {
       AUTH_CONSUMER_DECORATOR_SUBJECT_TYPES,
       options.subject?.types || []
     ),
-    SetPoliciesHandler(
-      ...(options.policies?.map(
-        (policy) => (ability: SubjectAbility, request: Request) => {
-          for (const attribute of Object.keys(policy.attributes || {})) {
-            if (typeof policy.attributes[attribute] !== 'string') {
-              continue;
-            }
-
-            /* Replacing the policy attributes from templates. */
-            const attr: string = policy.attributes[attribute] as string;
-
-            if (attr.startsWith('@Param::')) {
-              const paramName = attr.replace('@Param::', '');
-              policy.attributes[attribute] = request.params[paramName];
-            }
-
-            if (attr.startsWith('@Query::')) {
-              const paramName = attr.replace('@Query::', '');
-              policy.attributes[attribute] = request.query[paramName];
-            }
-
-            if (attr.startsWith('@Header::')) {
-              const paramName = attr.replace('@Header::', '');
-              policy.attributes[attribute] = request.headers[paramName];
-            }
-
-            if (attr.startsWith('@Body::')) {
-              const paramName = attr.replace('@Body::', '');
-              policy.attributes[attribute] = request.body[paramName];
-            }
-          }
-
-          return ability.can(
-            policy.action,
-            subject(policy.resource, { ...policy.attributes })
-          );
-        }
-      ) || [])
-    ),
+    SetPolicies(...(options.policies || [])),
     UseGuards(AuthorizationGuard, SubjectTypeGuard, PolicyGuard)
   );
 }
--- libs/casl/src/lib/policy.guard.ts
+++ libs/casl/src/lib/policy.guard.ts
@@ -2,8 +2,9 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { CASL_DECORATOR_POLICIES } from './casl.constants';
 import { CaslAbilityFactory } from './casl-ability.factory';
-import { PolicyHandler, SubjectAbility } from './casl.types';
+import { Policies, Policy, PolicyHandler, SubjectAbility } from './casl.types';
 import { Request } from 'express';
+import { subject } from '@casl/ability';
 
 @Injectable()
 export class PolicyGuard implements CanActivate {
@@ -19,11 +20,12 @@ export class PolicyGuard implements CanActivate {
    * @returns A boolean value.
    */
   canActivate(context: ExecutionContext): boolean {
-    const policyHandlers =
-      this.reflector.get<PolicyHandler[]>(
+    const policiesFromMetadata =
+      this.reflector.get<Policies>(
         CASL_DECORATOR_POLICIES,
         context.getHandler()
       ) || [];
+    const policyHandlers = this.computePolicyHandlers(policiesFromMetadata);
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const request: any = context.switchToHttp().getRequest();
@@ -44,12 +46,18 @@ export class PolicyGuard implements CanActivate {
         .reduce((base, next) => base.concat(next), []) || [];
 
     const ability = this.caslAbilityFactory.create(policies);
-
     return policyHandlers.every((handler) =>
       this.execPolicyHandler(handler, ability, request)
     );
   }
 
+  /**
+   * If the handler is a function, call it, otherwise call the handle method on the handler.
+   * @param {PolicyHandler} handler - The policy handler that is being executed.
+   * @param {SubjectAbility} ability - The ability that is being checked.
+   * @param {Request} request - The request object that was passed to the ability check.
+   * @returns A boolean value.
+   */
   private execPolicyHandler(
     handler: PolicyHandler,
     ability: SubjectAbility,
@@ -61,6 +69,66 @@ export class PolicyGuard implements CanActivate {
     return handler.handle(ability, request);
   }
 
+  /**
+   * It takes a list of policies and returns a list of functions that take an ability and a request and
+   * return a boolean
+   * @param {Policies} policies - Policies - This is the policies object that we defined in the
+   * previous section.
+   * @returns An array of functions that take an ability and a request and return a boolean.
+   */
+  private computePolicyHandlers(policies: Policies) {
+    return (
+      [...policies].map(
+        (policyFromMetadata) => (ability: SubjectAbility, request: Request) => {
+          // need to be a deep copy of the metadata to not mutate the original
+          const policy: Policy = JSON.parse(JSON.stringify(policyFromMetadata));
+
+          for (const attribute of Object.keys(policy.attributes || {})) {
+            if (typeof policy.attributes[attribute] !== 'string') {
+              continue;
+            }
+
+            /* Replacing the policy attributes from templates. */
+            const attr: string = policy.attributes[attribute] as string;
+
+            if (attr.startsWith('@Param::')) {
+              const paramName = attr.replace('@Param::', '');
+              policy.attributes[attribute] = request.params[paramName];
+            }
+
+            if (attr.startsWith('@Query::')) {
+              const paramName = attr.replace('@Query::', '');
+              policy.attributes[attribute] = request.query[paramName];
+            }
+
+            if (attr.startsWith('@Header::')) {
+              const paramName = attr.replace('@Header::', '');
+              policy.attributes[attribute] = request.headers[paramName];
+            }
+
+            if (attr.startsWith('@Body::')) {
+              const paramName = attr.replace('@Body::', '');
+              policy.attributes[attribute] = request.body[paramName];
+            }
+          }
+
+          return ability.can(
+            policy.action,
+            subject(policy.resource, { ...policy.attributes })
+          );
+        }
+      ) || []
+    );
+  }
+
+  /**
+   * It replaces the `@me` attribute with the user's internal identifier
+   * @param attributes - Record<string, unknown> - The attributes that are being sent to the
+   * authorization server.
+   * @param {Request} request - The request object that was sent to the API.
+   * @returns The attributes object with the @me template replaced with the internalIdentifier of the
+   * subject.
+   */
   private replaceAttributesTemplate(
     attributes: Record<string, unknown>,
     request: Request
