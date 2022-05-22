32ef898573c65ce22a33197d720565025deb0ee4
iverly <github@iverly.net>
2022-05-22 12:08:43 +0200

    fix: remove default image
    Signed-off-by: iverly <github@iverly.net>

--- apps/runner-service/src/app/app.module.ts
+++ apps/runner-service/src/app/app.module.ts
@@ -2,32 +2,12 @@ import { Module } from '@nestjs/common';
 import { RunnerDockerStrategy } from '@polycode/runner-docker-strategy';
 import { RunnerProviderModule } from '@polycode/runner-provider';
 import { ConfigModule } from '@nestjs/config';
-import { Language } from '@polycode/runner';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     RunnerProviderModule.register({
-      strategy: new RunnerDockerStrategy({
-        images: [
-          {
-            language: Language.JAVA,
-            image: 'polycode/java-runner:latest',
-          },
-          {
-            language: Language.JAVASCRIPT,
-            image: 'polycode/javascript-runner:latest',
-          },
-          {
-            language: Language.PYTHON,
-            image: 'polycode/python-runner:latest',
-          },
-          {
-            language: Language.RUST,
-            image: 'polycode/rust-runner:latest',
-          },
-        ],
-      }),
+      strategy: new RunnerDockerStrategy(),
     }),
   ],
   controllers: [],
